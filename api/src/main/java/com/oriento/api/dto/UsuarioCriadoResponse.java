77b27019c2d1dbfd2e4118c0756c4598a83d9ec9
cavalcante-html <leandrocavalcante2504@gmail.com>
2025-11-06 15:01:22 -0300

    feat: adicionado RateLimit e integração com o Gemini

--- api/src/main/java/com/oriento/api/dto/UsuarioCriadoResponse.java
+++ api/src/main/java/com/oriento/api/dto/UsuarioCriadoResponse.java
@@ -11,7 +11,7 @@ public record UsuarioCriadoResponse(
     
     public static UsuarioCriadoResponse erroJaCadastrado() {
         return new UsuarioCriadoResponse(
-                "Email ou CNPJ já cadastrado."
+                "Credenciais já cadastradas."
         );
     }
 
